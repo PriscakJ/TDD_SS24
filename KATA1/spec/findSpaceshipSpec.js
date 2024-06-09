2e5bdf7d6bea467403f2cb73558c409451e0e422
juraj <priscak.juraj@gmail.com>
2024-06-09 20:56:46 +0200

    ToDo1 OK

--- KATA1/spec/findSpaceshipSpec.js
+++ KATA1/spec/findSpaceshipSpec.js
@@ -2,6 +2,10 @@ describe('Find Spaceship', function() {
 	beforeEach(function() {
 		spaceship = new Spaceship();
 	});
+	it('should return "Spaceship lost forever" if the map is empty', function(){
+		const map ='';
+		expect(spaceship.findSpaceship(map)).toBe("Spaceship lost forever");
+	})
 
 });
 
