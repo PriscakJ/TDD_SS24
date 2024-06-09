7e188d2ecd8d6c4c17b0a1f29b844c5246769776
juraj <priscak.juraj@gmail.com>
2024-06-09 23:48:03 +0200

    ToDo 6 OK

--- KATA1/spec/findSpaceshipSpec.js
+++ KATA1/spec/findSpaceshipSpec.js
@@ -19,4 +19,13 @@ describe('Find Spaceship', function() {
         const map = "..........\n..........\n..........\n..........";
         expect(spaceship.findSpaceship(map)).toBe("Too few rows");
     }); 
+
+	it('should return "Too many or too few spaceships', function(){
+        const map = ".........X\n..........\n..........\n....X.....\n..........\n..........";
+        expect(spaceship.findSpaceship(map)).toBe("Count of spaceship incorrect");
+    }); 
+	it('should return "Spaceship found at [0,0]', function(){
+        const map = "..........\n..........\n..........\n..........\n..........\nX.........";
+        expect(spaceship.findSpaceship(map)).toEqual([0, 0]);
+    }); 
 });
--- KATA1/src/findSpaceship.js
+++ KATA1/src/findSpaceship.js
@@ -5,13 +5,38 @@ Spaceship.prototype.findSpaceship = function (map) {
     const numberOfRows = rows.length;
 
     let spaceshipPosition = null; 
+	let xCount = 0;
 
     if (numberOfRows > 6) {
         return "Too many rows"};
 
 	if (map != '' && numberOfRows<6){
 		return "Too few rows"};
-	
+
+	for (let row of rows) {
+		for (let char of row) {
+			if (char === 'X') {
+				xCount++;
+			}
+		}
+		}
+	if (map != '' && xCount != 1){
+		return "Count of spaceship incorrect";
+	}	
+
+	//ToDo 6
+	for (let y = 0; y < rows.length; y++) {
+        for (let x = 0; x < rows[y].length; x++) {
+            if (rows[y][x] === 'X') {
+                xCount++;
+                spaceshipPosition = [x, numberOfRows - y - 1];
+            }
+        }
+    }
+
+    if (xCount === 0) {
+        return "Spaceship lost forever";
+    }
 
     return spaceshipPosition ? spaceshipPosition : "Spaceship lost forever";
 };
