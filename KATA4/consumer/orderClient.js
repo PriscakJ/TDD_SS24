c52ca72738e4f925322f987672d8d4e56fda6195
juraj <priscak.juraj@gmail.com>
2024-06-26 10:03:02 +0200

    Failed Test  - hostname changed to 127.0.0.2

--- KATA4/consumer/orderClient.js
+++ KATA4/consumer/orderClient.js
@@ -1,7 +1,7 @@
 const request = require("superagent");
 const { Order } = require("./order");
 
-const hostname = "127.0.0.1"
+const hostname = "127.0.0.2"
 
 const fetchOrders = () => {
   return request.get(`http://${hostname}:${process.env.API_PORT}/orders`).then(
