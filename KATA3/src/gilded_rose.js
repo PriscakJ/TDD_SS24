8c2b8f37a49c2c2c7400c20f37f64acfdf243504
juraj <priscak.juraj@gmail.com>
2024-06-26 09:27:06 +0200

    Commit3->updateNormalItem function added

--- KATA3/src/gilded_rose.js
+++ KATA3/src/gilded_rose.js
@@ -35,52 +35,36 @@ function updateBackstagePass(item) {
   }
 }
 
+function updateNormalItem(item) {
+  if (item.quality > 0) {
+    item.quality = item.quality - 1;
+  }
+}
+
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name == 'Aged Brie') {
       updateAgedBrie(items[i]);
     } else if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
       updateBackstagePass(items[i]);
-    } else if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1;
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1;
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-        }
-      }
+    } else if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      updateNormalItem(items[i]);
     }
+
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
       items[i].sell_in = items[i].sell_in - 1;
     }
+
     if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1;
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
-        }
-      } else {
+      if (items[i].name == 'Aged Brie') {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1;
+        }
+      } else if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        items[i].quality = 0;
+      } else if (items[i].quality > 0) {
+        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          items[i].quality = items[i].quality - 1;
         }
       }
     }
